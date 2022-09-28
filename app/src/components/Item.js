e486b2b8ed122c2820d362fa620d2ad37109ae3f
Francisco Jose Garcia <francisco@goltratec.com>
2022-09-28 16:30:29 +0200

    .

--- app/src/components/Item.js
+++ app/src/components/Item.js
@@ -2,13 +2,22 @@ import '../global.css';
 import './Item.css';
 
 
-const Item = ({ unread }) => {
-    console.log(unread)
+const Item = ({ item }) => {
+    const pointUnread = () => {
+        if (item.unread) {
+            return <img className='unreadPoint' src={require('../images/punto.png')}></img>
+        }
+    }
     return (
-
-        <div className={`item ${unread ? 'item-unread' : 'item-read'}`}>
-           <img  src={require('../images/avatar-angela-gray.webp')}></img>
-            Item
+        <div className={`item ${item.unread ? 'item-unread' : 'item-read'}`}>
+            <img className='avatar' src={require('../images/' + item.image)}></img>
+            <div>
+                <div className='message-container'>
+                    <p className='message' dangerouslySetInnerHTML={{ __html: item.message }}></p>
+                    {pointUnread()}
+                </div>
+                <p className='time'>5m ago</p>
+            </div>
         </div>
     )
 }
--- app/src/components/NotificationsList.js
+++ app/src/components/NotificationsList.js
@@ -4,17 +4,21 @@ import Notifications from './Notifications';
 import Item from './Item';
 
 
-const NotificationsList=()=>{
-    return(
+const NotificationsList = () => {
+    return (
         <div className='container'>
-            <div class='content'> 
-                <Notifications unreadItems="3"/>
+            <div class='content'>
+                <Notifications unreadItems="3" />
                 <div>
-                    <Item unread={true}></Item>
-                    <Item unread={true}></Item>
-                    <Item unread={false}></Item>
-                    <Item unread={true}></Item>
-                    <Item unread={false}></Item>
+                    <Item item={{
+                        unread: true,
+                        image: 'avatar-angela-gray.webp',
+                        message: '<strong>Mark Webber</strong> <span className="action">reacted to your recent post</span> <strong>My</strong>'
+                        }}></Item>
+                    <Item item={{ unread: true, image: 'avatar-angela-gray.webp',message: '' }}></Item>
+                    <Item item={{ unread: false, image: 'avatar-angela-gray.webp',message: '' }}></Item>
+                    <Item item={{ unread: true, image: 'avatar-angela-gray.webp',message: '' }}></Item>
+                    <Item item={{ unread: false, image: 'avatar-angela-gray.webp',message: '' }}></Item>
                 </div>
             </div>
         </div>
