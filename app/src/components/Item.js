80c8d7f7e54168bbd1fd8ec4744b1a148ddd6a8a
Francisco Garcia <franciscogarcia@Franciscos-MacBook-Air.local>
2022-10-02 21:25:43 +0200

    .

--- app/src/components/Item.js
+++ app/src/components/Item.js
@@ -1,32 +1,44 @@
+import { useEffect, useState } from 'react';
 import '../global.css';
 import './Item.css';
 import PrivateMessage from './PrivateMessage';
 
 
-const Item = ({ item, toogle }) => {
+const Item = ({ item,unread }) => {
     const pointUnread = () => {
-        if (item.unread) {
-            return <img className='unreadPoint' src={require('../images/punto.png')}></img>
+        if (i.unread) {
+            return <img onClick={setRead} className='unreadPoint' src={require('../images/punto.png')}></img>
         }
     }
     const privateMessage = () => {
-        if (item.isPrivate) {
-            return <PrivateMessage text={item.privateMessage}></PrivateMessage>;
+        if (i.isPrivate) {
+            return <PrivateMessage text={i.privateMessage}></PrivateMessage>;
         }
     }
+
+    useEffect(() => {
+        console.log('useEffect Item', i.unread);
+    })
+    const [i, setItem] = useState(item);
+
+    const setRead = () => { 
+        setItem({...i, unread : false});
+     };
+
     return (
         <section>
-            <div className={`item ${item.unread ? 'item-unread' : 'item-read'}`}>
-                <img className='avatar' src={require('../images/' + item.image)}></img>
+            <div key={i.key} className={`item ${i.unread ? 'item-unread' : 'item-read'}`}>
+                <img className='avatar' src={require('../images/' + i.image)}></img>
                 <div>
                     <div className='message-container'>
+                        -- {JSON.stringify(unread)}
                         <p className='message'>
-                            <span className="activate">{item.user}</span>
-                            <span className='action'>{" " + item.action + " "} </span>
-                            <span className="activate">{item.actionTo}</span></p>
-                        <span onClick={()=>toogle(item)} className="activate">{pointUnread()}</span>
+                            <span className="activate">{i.user}</span>
+                            <span className='action'>{" " + i.action + " "} </span>
+                            <span className="activate">{i.actionTo}</span></p>
+                        <span className="activate">{pointUnread()}</span>
                     </div>
-                    <p className='time'>5m ago</p>
+                    <p className='time'>{i.time}</p>
                 </div>
             </div>
             <div>
--- app/src/components/NotificationsList.js
+++ app/src/components/NotificationsList.js
@@ -3,24 +3,47 @@ import './NotificationsList.css'
 import Notifications from './Notifications';
 import Item from './Item';
 import data from '../data/messages.json';
+import { useEffect, useState } from 'react';
 
 
 const NotificationsList = () => {
-    const toogle = (item)=>{
-        console.log('item',item);
-        /*item.unread = !item.unread;*/
-    };
-console.log(data);
+    
+    const [messages, setMessages] = useState(data);
+    const [unreads, setUnreads]= useState();
+
+
+
+    const markAllRead = () => {
+        setMessages((msgs)=>{
+            msgs.map(m=>m.unread=false);
+            return msgs;
+        })
+        countUnread();
+        console.log('mark', messages);
+    }
+    
+    const countUnread=()=>{
+        const withUnread = messages.filter(m=>m.unread===true);
+        console.log('withUnread',withUnread);
+        setUnreads(withUnread.length);
+    }
+
+    useEffect(()=>{
+        countUnread();
+        console.log('useEffect NotificationList', messages)
+    },[]);
+    
+
     return (
         <div className='container'>
-            <div class='content'>
+            <div className='content'>
                 <div className="notification-display">
-                    <Notifications unreadItems="3"/>
-                    <span className="activate mark-all ">Mark all as read</span>
+                    <Notifications unreadItems={unreads} />
+                    <span className="activate mark-all " onClick={markAllRead}>Mark all as read</span>
                 </div>
                 <div>
                     {
-                        data.map((d)=> <Item key={d.id} item={d} toogle-read={(e)=>toogle(e)}></Item> )
+                        messages.map((d) => <Item key={d.id} item={d} unread={d.unread}></Item>)
                     }
                 </div>
             </div>
